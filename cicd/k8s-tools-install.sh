ab43fc20e6299054e16392f118be8ad7049cf5f1
Fred B <fredbitenyo@gmail.com>
2023-11-28 00:50:21 +0200

    adding script for installing k8s cli

---
+++ cicd/k8s-tools-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "install eksctl in Ubuntu"
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+echo "install kubectl in Ubuntu"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
